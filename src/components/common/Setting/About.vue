f587a30bccffb89d3e83a6f2845b1eed085ab405
wuzhanfly <wuzhanfly@gmail.com>
2024-03-23 13:04:52 +0800

    upup

--- src/components/common/Setting/About.vue
+++ src/components/common/Setting/About.vue
@@ -31,7 +31,7 @@ async function fetchConfig() {
     loading.value = true
     // const { data } = await fetchChatConfig<ConfigState>()
     // config.value = data
-    
+
 
     const dd= await gptUsage();
     config.value= {usage:dd.usage?`${dd.usage}`:'-'
@@ -59,7 +59,7 @@ const getLastFrom= ()=>{
 }
 onMounted( () => {
   fetchConfig();
-  
+
   let t = getLastFrom();
   if(t){
      st.value.lastVersion = t ;
@@ -80,14 +80,6 @@ const  isShow = computed(()=>{
 <template>
   <NSpin :show="loading">
     <div class="p-4 space-y-4">
-      <h2 class="text-xl font-bold">
-        Version - {{ pkg.version }}
-        <a class="text-red-500" href="https://github.com/Dooy/chatgpt-web-midjourney-proxy" target="_blank" v-if=" isShow  "> ({{ $t('mj.findVersion') }} {{ st.lastVersion }})</a>
-        <a class="text-gray-500" href="https://github.com/Dooy/chatgpt-web-midjourney-proxy" target="_blank" v-else-if="st.lastVersion"> ({{ $t('mj.yesLastVersion') }})</a>
-      </h2>
-      <div class="p-2 space-y-2 rounded-md bg-neutral-100 dark:bg-neutral-700">
-        <p v-html="$t('mj.infoStar')"></p>
-      </div>
       <p>{{ $t("setting.api") }}：{{ config?.apiModel ?? '-' }}</p>
       <p v-if="isChatGPTAPI" class=" flex items-center justify-between">
         <div>
@@ -103,7 +95,7 @@ const  isShow = computed(()=>{
       <p v-if="!isChatGPTAPI">
         {{ $t("setting.reverseProxy") }}：{{ config?.reverseProxy ?? '-' }}
       </p>
-       
+
       <!-- <p>{{ $t("setting.timeout") }}：{{ config?.timeoutMs ?? '-' }}</p>  -->
       <!-- <p>{{ $t("setting.socks") }}：{{ config?.socksProxy ?? '-' }}</p>
       <p>{{ $t("setting.httpsProxy") }}：{{ config?.httpsProxy ?? '-' }}</p> -->
--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Dooy/chatgpt-web-midjourney-proxy/main/src/assets/avatar.jpg',
+      avatar: 'https://avatars.githubusercontent.com/u/14866386?v=4',
       name:  t('mjset.sysname'),//'AI绘图',
-      description: 'Star on <a href="https://github.com/Dooy/chatgpt-web-midjourney-proxy" class="text-blue-500" target="_blank" >GitHub</a>',
+      description: 'Star on <a href="https://github.com/wuzhanfly" class="text-blue-500" target="_blank" >GitHub</a>',
     },
   }
 }
